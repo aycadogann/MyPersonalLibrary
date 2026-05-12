f6acff99e3a8ca70a8417cb2f4ef9879ad6ac350
Ayca Dogan <dogan.aycaa@gmail.com>
2026-05-12 16:31:10 +0300

    Add project files.

---
+++ MyPersonalLib.UI/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿
+namespace MyPersonalLib.UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_ID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_BookName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Author = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmb_Genre = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.chk_IsRead = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dgw_BookList = new System.Windows.Forms.DataGridView();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.num_Rate = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_BookList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_Rate)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt_ID
+            // 
+            this.txt_ID.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_ID.Location = new System.Drawing.Point(168, 39);
+            this.txt_ID.Name = "txt_ID";
+            this.txt_ID.ReadOnly = true;
+            this.txt_ID.Size = new System.Drawing.Size(253, 34);
+            this.txt_ID.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(60, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(60, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Kitap Adı:";
+            // 
+            // txt_BookName
+            // 
+            this.txt_BookName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_BookName.Location = new System.Drawing.Point(168, 79);
+            this.txt_BookName.Name = "txt_BookName";
+            this.txt_BookName.Size = new System.Drawing.Size(253, 34);
+            this.txt_BookName.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(60, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 28);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Yazar:";
+            // 
+            // txt_Author
+            // 
+            this.txt_Author.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_Author.Location = new System.Drawing.Point(168, 119);
+            this.txt_Author.Name = "txt_Author";
+            this.txt_Author.Size = new System.Drawing.Size(253, 34);
+            this.txt_Author.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(60, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 28);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Kitap Türü:";
+            // 
+            // cmb_Genre
+            // 
+            this.cmb_Genre.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmb_Genre.FormattingEnabled = true;
+            this.cmb_Genre.Location = new System.Drawing.Point(168, 159);
+            this.cmb_Genre.Name = "cmb_Genre";
+            this.cmb_Genre.Size = new System.Drawing.Size(253, 36);
+            this.cmb_Genre.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(503, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 28);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Başlama Tarihi:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Segoe Fluent Icons", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(641, 45);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(253, 34);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CalendarFont = new System.Drawing.Font("Segoe Fluent Icons", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(641, 85);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(253, 34);
+            this.dateTimePicker2.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(503, 85);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(129, 28);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Bitirme Tarihi:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(503, 125);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 28);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Durum:";
+            // 
+            // chk_IsRead
+            // 
+            this.chk_IsRead.AutoSize = true;
+            this.chk_IsRead.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.chk_IsRead.Location = new System.Drawing.Point(641, 125);
+            this.chk_IsRead.Name = "chk_IsRead";
+            this.chk_IsRead.Size = new System.Drawing.Size(104, 32);
+            this.chk_IsRead.TabIndex = 16;
+            this.chk_IsRead.Text = "Okundu";
+            this.chk_IsRead.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(503, 163);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 28);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Puan:";
+            // 
+            // dgw_BookList
+            // 
+            this.dgw_BookList.BackgroundColor = System.Drawing.Color.White;
+            this.dgw_BookList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgw_BookList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgw_BookList.Location = new System.Drawing.Point(3, 18);
+            this.dgw_BookList.Name = "dgw_BookList";
+            this.dgw_BookList.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dgw_BookList.RowHeadersWidth = 51;
+            this.dgw_BookList.RowTemplate.Height = 24;
+            this.dgw_BookList.Size = new System.Drawing.Size(952, 234);
+            this.dgw_BookList.TabIndex = 19;
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.BackgroundImage = global::MyPersonalLib.UI.Properties.Resources.delete;
+            this.btn_Delete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_Delete.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btn_Delete.Location = new System.Drawing.Point(529, 215);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(50, 50);
+            this.btn_Delete.TabIndex = 22;
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.BackgroundImage = global::MyPersonalLib.UI.Properties.Resources.update;
+            this.btn_Update.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_Update.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btn_Update.Location = new System.Drawing.Point(419, 215);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(50, 50);
+            this.btn_Update.TabIndex = 21;
+            this.btn_Update.UseVisualStyleBackColor = true;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.BackgroundImage = global::MyPersonalLib.UI.Properties.Resources.save;
+            this.btn_Add.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_Add.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btn_Add.Location = new System.Drawing.Point(309, 215);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(50, 50);
+            this.btn_Add.TabIndex = 20;
+            this.btn_Add.UseVisualStyleBackColor = true;
+            // 
+            // num_Rate
+            // 
+            this.num_Rate.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.num_Rate.Location = new System.Drawing.Point(641, 163);
+            this.num_Rate.Name = "num_Rate";
+            this.num_Rate.Size = new System.Drawing.Size(48, 34);
+            this.num_Rate.TabIndex = 23;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.dgw_BookList);
+            this.groupBox1.Location = new System.Drawing.Point(3, 291);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(958, 255);
+            this.groupBox1.TabIndex = 24;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kitap Listesi";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.txt_BookName);
+            this.groupBox2.Controls.Add(this.txt_ID);
+            this.groupBox2.Controls.Add(this.num_Rate);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btn_Delete);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.btn_Update);
+            this.groupBox2.Controls.Add(this.txt_Author);
+            this.groupBox2.Controls.Add(this.btn_Add);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.chk_IsRead);
+            this.groupBox2.Controls.Add(this.cmb_Genre);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.dateTimePicker2);
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(3, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(958, 283);
+            this.groupBox2.TabIndex = 25;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Kitap Bilgiler";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(963, 549);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My Personal Library";
+            ((System.ComponentModel.ISupportInitialize)(this.dgw_BookList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num_Rate)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_ID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_BookName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Author;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmb_Genre;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox chk_IsRead;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dgw_BookList;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.NumericUpDown num_Rate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
