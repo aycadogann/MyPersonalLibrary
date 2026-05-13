105fa275e7da20c17325245c1c4ecef40a12eec2
Ayca Dogan <dogan.aycaa@gmail.com>
2026-05-13 08:52:12 +0300

    DataAccess katmani eklendi ve Veritabani baglantisi kuruldu

---
+++ MyPersonalLib.DataAccess/DbConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPersonalLib.DataAccess
+{
+    public static class DbConnection
+    {
+        
+        public static  SqlConnection _connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MyPersonalLibrary;Integrated Security=True");
+    }
+}
